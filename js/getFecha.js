df60c365429ef48f60666723a04fd517d9fc6f77
carlosDev22 <71095613+carlosDev22@users.noreply.github.com>
2021-03-27 23:52:14 -0600

    index, vue y css

---
+++ js/getFecha.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var fecha = new Date();
+    var mes = fecha.getMonth()+1;
+    var dia = fecha.getDate(); 
+    var ano = fecha.getFullYear();
+    if(dia<10){
+          dia='0'+dia;
+      }
+    if(mes<10){
+          mes='0'+mes 
+      }
+  
+    document.getElementById('checkin').min= ano + "-" + mes + "-" + dia; 
+    document.getElementById('checkin').value= ano + "-" + mes + "-" + dia;
+  }
